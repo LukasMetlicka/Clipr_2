2ce591587f19a8534099392887ee22f2fdd886e4
Lukas Metlicka <lmetlicka@g.harvard.edu>
2014-08-06 09:26:59 -0400

    paranoid commit

--- app/views/add.blade.php
+++ app/views/add.blade.php
@@ -1,27 +1,28 @@
 @extends("_master")
+
 @section('title')
 Add a clip
 @stop
+
 @section('head')
 	<link type="text/css" rel="stylesheet" href="styles/add.css">
 @stop
 
 @section('body')
-	
-	{{ Form::open(array('url' => '/add', 'method' => 'POST')) }}
-		<div class="content">
-			<div class="text">
-				{{ Form::label("clip_text", "the content of the clip") }}
-				{{ Form::text('text') }}
-			</div>
-			<div class="tags">
-				{{ Form::label("clip_tags", "tags to sort through clips more easily!") }}
-				{{ Form::text("tags") }}
-			</div>
-			<div class="submit">
-				{{ Form::submit('Submit!', array('class' => 'submit')); }}
-			</div>
-		</div>
-	{{ Form::close() }}
+	<div class="content">
+		{{ Form::open(array('url' => '/add', 'method' => 'POST')) }}
+				<div class="text">
+					<h2>Put the text for your clip here! make sure you only use * characters!</h2>
+					{{ Form::textarea('text') }}
+				</div>
+				<div class="tags">
+					<h2>Put a tag here! preface it with a #, and make sure you don't put spaces in it!</h2>
+					{{ Form::text("tags") }}
+				</div>
+				<div class="submit">
+					{{ Form::submit('Submit!', array('class' => 'submit')); }}
+				</div>
+		{{ Form::close() }}
+	</div>
 	
 @stop
